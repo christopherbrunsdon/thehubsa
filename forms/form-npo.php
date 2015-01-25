658dffa247849241d13903dbfd90b241b932f9a0
Christopher Brunsdon <christopher@brunsdon.co.za>
2015-01-25 19:31:37 +0000

    Updated form as per Sandy

--- forms/form-npo.php
+++ forms/form-npo.php
@@ -32,6 +32,8 @@ class form_npo
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<!-- jquery -->
+		<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
 		<?php
 	}
 
@@ -52,12 +54,17 @@ class form_npo
 			<h2>NAME OF ORGANISATION</h2>
 
 			<div class="form-group">
-				<label for="">NPO registration number &amp; date of registration</label>
-				<input type="text" class="form-control" id="" placeholder="NPO registration number &amp; date of registration" />
+				<label for="">NPO name</label>
+				<input type="text" class="form-control" id="" placeholder="NPO name" />
 			</div>
 
 			<div class="form-group">
-				<label for="">Other registration numbers </label>
+				<label for="">NPO registration number</label>
+				<input type="text" class="form-control" id="" placeholder="NPO registration number" />
+			</div>
+
+			<div class="form-group">
+				<label for="">Other registration numbers and descriptions (not required) </label>
 				<input type="text" class="form-control" id="" placeholder="Other registration numbers" />
 			</div>
 
@@ -77,17 +84,17 @@ class form_npo
 			<!-- contact details -->
 			<h2>CONTACT DETAILS</h2>
 			<div class="form-group"> 
-				<label for="">Contact person </label>
+				<label for="">Contact person  (Title, First Name, Surname)</label>
 				<input type="text" class="form-control" id="" placeholder="Contact person " />
 			</div>
 
 			<div class="form-group"> 
-				<label for="">Telephone number </label>
+				<label for="">Telephone number  (example: +2721800000 no spaces)"</label>
 				<input type="text" class="form-control" id="" placeholder="Telephone number " />
 			</div>
 
 			<div class="form-group"> 
-				<label for="">Cell number </label>
+				<label for="">Cell number  (example 0822222222222 no spaces)</label>
 				<input type="text" class="form-control" id="" placeholder="Cell number " />
 			</div>
 
@@ -102,7 +109,7 @@ class form_npo
 			</div>
 
 			<div class="form-group"> 
-				<label for="">Preferred link for listing </label>
+				<label for="">Preferred link for listing  (if not your home page as above)</label>
 				<input type="text" class="form-control" id="" placeholder="Preferred link for listing " />
 			</div>
 
@@ -118,43 +125,74 @@ class form_npo
  -->
 		<?php for($i=1; $i <= 5; $i++): ?>
 			<div class="form-group"> 
-				<?php if($i == 1) : ?><label for="">Services offered</label><?php endif ?>
-				<select  class="form-control" id="">
-					<option>-- Services offered --</option>
-					<option value="">Adult abuse victim support</option>
-					<option value="">Adult education</option>
-					<option value="">Adult rape victim support</option>
-					<option value="">Animal abuse intervention</option>
-					<option value="">Child abuse victim support</option>
-					<option value="">Child rape victim support</option>
-					<option value="">Crisis pregnancy support</option>
-					<option value="">ECD education</option>
-					<option value="">Environmental projects</option>
-					<option value="">Feeding scheme</option>
-					<option value="">Food gardens</option>
-					<option value="">HIV and AIDS intervention</option>
-					<option value="">Literacy scheme</option>
-					<option value="">Lost and found animals</option>
-					<option value="">Lost and found children/adults</option>
-					<option value="">Skills training </option>
-					<option value="">Substance abuse</option>
-					<option value="">Support for homeless</option>
-					<option value="">Support for new mothers</option>
-					<option value="">Support for the disabled</option>
-					<option value="">Support for the elderly</option>
-					<option value="">Support for the terminal</option>
-					<option value="">Tertiary education</option>
-					<option value="">Wildlife</option>
-				</select>
+				<?php if($i == 1) : ?><label for="">Services offered (in order of importance)</label><?php endif ?>
+
+				<div class="input-group">
+					<div class="input-group-addon"><?= $i ?>.</div>
+
+					<select  class="form-control" id="services_<?= $i; ?>">
+						<option>-- Services offered --</option>
+						<option value="">Adult abuse victim support</option>
+						<option value="">Adult education</option>
+						<option value="">Adult rape victim support</option>
+						<option value="">Animal abuse intervention</option>
+						<option value="">Child abuse victim support</option>
+						<option value="">Child rape victim support</option>
+						<option value="">Crisis pregnancy support</option>
+						<option value="">ECD education</option>
+						<option value="">Environmental projects</option>
+						<option value="">Feeding scheme</option>
+						<option value="">Food gardens</option>
+						<option value="">HIV and AIDS intervention</option>
+						<option value="">Literacy scheme</option>
+						<option value="">Lost and found animals</option>
+						<option value="">Lost and found children/adults</option>
+						<option value="">Skills training </option>
+						<option value="">Substance abuse</option>
+						<option value="">Support for homeless</option>
+						<option value="">Support for new mothers</option>
+						<option value="">Support for the disabled</option>
+						<option value="">Support for the elderly</option>
+						<option value="">Support for the terminal</option>
+						<option value="">Tertiary education</option>
+						<option value="">Wildlife</option>
+						<option value="-- Other --" id="services_other_<?= $i; ?>">-- Other (Please indicate) --</option>					
+					</select>
+				</div>
+
+				<div style="display: none;" id="service_other_input_<?= $i; ?>">
+					<div class="input-group">
+						<div class="input-group-addon">Other:</div>
+						<input  type="text" class="form-control"  placeholder="Other (Please inidcate)" />
+					</div>
+				</div>
 			</div>
+
+			<script type="text/javascript">
+				$('#services_<?= $i; ?>').change(function(){
+					var selected_item = $(this).val()
+					if(selected_item == "-- Other --"){ // 'Other' 
+					    $('#service_other_input_<?= $i; ?>').show(); //show textbox if Other is selected
+					}else{
+					    $('#service_other_input_<?= $i; ?>').hide(); //Hide textbox if anything else is selected
+					}
+				});
+			</script>
 		<?php endfor; ?>
+
+
+			<div class="form-group">
+				<label for="">Give a short description (400 characters or less) of your organisation</label>
+				<textarea rows=4 type="text" class="form-control" id="" placeholder="Give a short description (400 characters or less) of your organisation"></textarea>
+			</div>
+
 
 
 			<h2>SKILLS/RESOURCE BANK</h2>
 
 			<div class="form-group">
-				<label for="">What can your organisation contribute? </label>
-				<textarea rows=4 type="text" class="form-control" id="" placeholder="What can your organisation contribute?"></textarea>
+				<label for="">What can your organisation offer to the community (eg training, office space, haircuts) </label>
+				<textarea rows=4 type="text" class="form-control" id="" placeholder="What can your organisation offer to the community (eg training, office space, haircuts)"></textarea>
 			</div>
 
 			<div class="form-group">
@@ -163,17 +201,54 @@ class form_npo
 			</div>
 
 			<div class="form-group">
-				<label for="">Needs / Wish list</label>
-				<textarea rows=4 type="text" class="form-control" id="" placeholder="Needs / Wish list"></textarea>
+				<label for="">Needs list</label>
+				<textarea rows=4 type="text" class="form-control" id="" placeholder="Needs list"></textarea>
 			</div>
+
+			<div class="form-group">
+				<label for=""> Wish list</label>
+				<textarea rows=4 type="text" class="form-control" id="" placeholder="Wish list"></textarea>
+			</div>
+
+			<p>
+				The HUB SA Helderberg reserves the right to accept/decline any affiliation.
+			</p>
+			<p>
+				The HUB SA Helderberg, including its committee and other members and any other person acting on behalf of The HUB SA Helderberg (“The HUB SA Helderberg”) is hereby absolved from all and any liability or claims of whatsoever nature for loss or damage (including any special or consequential damages) arising directly or indirectly out of or flowing from any services or assistance rendered or function fulfilled or activity carried out to any person or party by The HUB SA Helderberg , provided that The HUB SA Helderberg acted in good faith and in a reasonable manner.
+			</p>
+			<p>
+				Commitment fee: R200
+			</p>
+			<p>
+				Applicable on submission of registration form (non-refundable)
+			</p>
+			<p>
+				Thereafter an annual fee will apply. The fee is about giving a monetary value to the commitment you are making.
+			</p>
+			<p>
+				<h4>Bank details: </h4> <br />
+				<strong>Name: </strong> The HUB SA Helderberg <br />
+				<strong>Bank: </strong> FNB <br />
+				<strong>ACCOUNT TYPE: </strong> Business Cheque <br />
+				<strong>Account number: </strong> 62501541339 <br />
+				<strong>Branch code: </strong> 200912 – Somerset Mall <br />
+				<strong>Confirmation of payment:</strong> lynettepullen@gmail.com
+			</p>
 
 			<div class="checkbox">
 			    <label>
-			      <input type="checkbox"> Payment Made
+			      <input onclick="$('#btn-submit').prop('disabled', false).addClass('btn-primary').removeClass('btn-default');" type="checkbox"> 
+			      Payment Made via EFT
+			    </label>
+			 </div>
+			<div class="checkbox">
+			    <label>
+			      <input onclick="$('#btn-submit').prop('disabled', false).addClass('btn-primary').removeClass('btn-default');" type="checkbox"> 
+			      Payment Made via Deposit
 			    </label>
 			 </div>
 
-			<button type="submit" class="btn btn-default">Submit</button>
+			<button id="btn-submit" disabled type="submit" class="btn btn-default">Submit</button>
 		</form>	
 		<?php
 	}
