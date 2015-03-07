8b02e24a27c8003d34953a0a42628a3cf3eefa2d
Christopher Brunsdon <christopher@brunsdon.co.za>
2015-03-07 09:52:38 +0000

    wip

--- tests/test_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class WP_Is_Email_Test extends WP_UnitTestCase {
-
-	function test_is_email_only_letters_with_dot_com_domain() {
-		$this->assertEquals( 'nb@nikolay.com',  'nb@nikolay.com' );
-	}
-
-}
