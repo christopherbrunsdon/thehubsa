001d8a6851c144a84647a59695160411990a1113
Christopher Brunsdon <christopher@brunsdon.co.za>
2015-03-07 18:13:50 +0000

    wip

--- install/create_tables.php
+++ install/create_tables.php
@@ -1,47 +1,47 @@
 <?php
-//defined('ABSPATH') or die("No script kiddies please!");
-
-// format date.version
-DEFINE('THEHUBSA_DB_VERSION', 20150302);
-
-/**
- * Setup
- * http://codex.wordpress.org/Creating_Tables_with_Plugins
- */
-
-function thehubsa_install()
-{
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-	// +++ list the table here
-
-	dbDelta(model_thehub_membership_types::get_create_table());
-	dbDelta(model_thehub_memberships::get_create_table());
-
-	dbDelta(model_thehub_npos::get_create_table()); //error_log(model_thehub_npos::get_create_table());
-	dbDelta(model_thehub_npo_service_types::get_create_table());
-	dbDelta(model_thehub_npo_services::get_create_table());
-
-	//+++ 
-
-	if(get_option('thehubsa_db_version') === False) {
-		add_option('thehubsa_db_version', THEHUBSA_DB_VERSION );
-	} else {
-		update_option('thehubsa_db_version', THEHUBSA_DB_VERSION );
-	}
-}
-
-/**
- * Used for upgrades
- */
-
-function myplugin_update_db_check() {
-    if (get_site_option( 'thehubsa_db_version' ) != THEHUBSA_DB_VERSION ) 
-    {
-        thehubsa_install();
-        thehubsa_install_data();
-    }
-}
-
-
-// lets install
+////defined('ABSPATH') or die("No script kiddies please!");
+//
+//// format date.version
+//DEFINE('THEHUBSA_DB_VERSION', 20150302);
+//
+///**
+// * Setup
+// * http://codex.wordpress.org/Creating_Tables_with_Plugins
+// */
+//
+//function thehubsa_install()
+//{
+//	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+//
+//	// +++ list the table here
+//
+//	dbDelta(model_thehub_membership_types::get_create_table());
+//	dbDelta(model_thehub_memberships::get_create_table());
+//
+//	dbDelta(model_thehub_npos::get_create_table()); //error_log(model_thehub_npos::get_create_table());
+//	dbDelta(model_thehub_npo_service_types::get_create_table());
+//	dbDelta(model_thehub_npo_services::get_create_table());
+//
+//	//+++
+//
+//	if(get_option('thehubsa_db_version') === False) {
+//		add_option('thehubsa_db_version', THEHUBSA_DB_VERSION );
+//	} else {
+//		update_option('thehubsa_db_version', THEHUBSA_DB_VERSION );
+//	}
+//}
+//
+///**
+// * Used for upgrades
+// */
+//
+//function myplugin_update_db_check() {
+//    if (get_site_option( 'thehubsa_db_version' ) != THEHUBSA_DB_VERSION )
+//    {
+//        thehubsa_install();
+//        thehubsa_install_data();
+//    }
+//}
+//
+//
+//// lets install
--- thehubsa-plugin.php
+++ thehubsa-plugin.php
@@ -1,45 +1,136 @@
 <?php
+/**
+ * Plugin Name: TheHubSA.org.za
+ * Plugin URI: https://github.com/christopherbrunsdon/thehubsa
+ * Description: Custom Wordpress Plugin for TheHubSA.org.za. Open source code, please contribute at https://github.com/christopherbrunsdon/thehubsa
+ * Version: 1.03-20150307
+ * Author: Christopher Brunsdon
+ * Author URI: http://www.brunsdon.co.za
+ * Requires at least: 4.0
+ * Tested up to: 4.1
+ *
+ * Text Domain: thehubsa
+ */
 
 defined('ABSPATH') or die("No script kiddies please!");
 
-/*
-Plugin Name: TheHubSA.org.za
-Plugin URI: https://github.com/christopherbrunsdon/thehubsa
-Description: Custom Wordpress Plugin for TheHubSA.org.za. Open source code, please contribute at https://github.com/christopherbrunsdon/thehubsa
-Version: 1.03-20150302
-Author: Christopher Brunsdon
-Author URI: http://www.brunsdon.co.za
-*/
+if (!class_exists('TheHubSAClass')):
 
-// +++ models
+/**
+ * Main the hub class
+ *
+ */
+final class TheHubSAClass
+{
+    /**
+     * @var string
+     */
+    public $version = '1.03-20150307';
 
-require_once('models/membership_types.php');
-require_once('models/memberships.php');
-require_once('models/npo_service_types.php');
-require_once('models/npo_services.php');
-require_once('models/npos.php');
+    /**
+     * @var null
+     */
+    protected static $_instance = null;
 
-// +++ forms
 
-require_once('forms/form-join.php');
-require_once('forms/form-npo.php');
-require_once('forms/form-business.php');
+    /**
+     * @return null|TheHubSAClass
+     */
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
 
-// +++ controllers (views)
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->define_constants();
+        $this->includes();
+        $this->init_hooks();
+    }
 
-require_once('controllers/npo.php');
+    /**
+     *
+     */
+    public function includes()
+    {
+        require_once('models/membership_types.php');
+        require_once('models/memberships.php');
+        require_once('models/npo_service_types.php');
+        require_once('models/npo_services.php');
+        require_once('models/npos.php');
 
-// +++ database
+        // +++ forms
 
-require_once('install/add_data.php');
-require_once('install/create_tables.php');
+        require_once('forms/form-join.php');
+        require_once('forms/form-npo.php');
+        require_once('forms/form-business.php');
 
-register_activation_hook( __FILE__, 'thehubsa_install' );
-register_activation_hook( __FILE__, 'thehubsa_install_data' );
-add_action( 'plugins_loaded', 'myplugin_update_db_check' );
+        // +++ controllers (views)
 
-// +++ admin page
+        require_once('controllers/npo.php');
 
-require_once('admin/admin.php');
+        // +++ database
+
+        require_once('install/add_data.php');
+        require_once('install/create_tables.php');
+
+        // +++ admin page
+
+        require_once('admin/admin.php');
+        require_once('includes/class-wc-install.php');
+    }
+
+    /**
+     * Define the constants here
+     */
+    private function define_constants()
+    {
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    private function define($name, $value)
+    {
+        if (!defined($name)) {
+            define($name, $value);
+        }
+    }
+
+    /**
+     *
+     */
+    private function init_hooks()
+    {
+        register_activation_hook(__FILE__, array('TheHubSA_Install', 'install'));
+        add_action('init', array($this, 'init'), 0);
+        add_action( 'init', array( 'WC_Shortcodes', 'init' ) );
+    }
+
+    /**
+     *
+     */
+    public function init()
+    {
+        // init code comes here ....
+    }
+
+} // class
+endif;
+
+/**
+ * @return null|TheHubSA
+ */
+function TheHubSa()
+{
+    return TheHubSAClass::instance();
+}
 
 // [eof]
